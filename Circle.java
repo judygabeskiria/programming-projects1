4b058e6d51aa7ac5f9cdccced01abbffb341342f
judygabeskiria <gabeskiriai@student.mville.edu>
2017-07-09 14:46:08 +0400

    first commit

---
+++ Circle.java
@@ -0,0 +1,14 @@
+/**
+ * Auto Generated Java Class.
+ */
+public class Circle implements Shape {
+  
+  Posn n;
+  int radius;
+  public Circle( Posn n, int radius)
+  {
+    this.n=n;
+    this.radius=radius;
+  }
+  
+}
