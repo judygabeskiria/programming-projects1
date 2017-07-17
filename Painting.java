781436f4c63a625ed77272b191a9a9d936f74c27
judygabeskiria <gabeskiriai@student.mville.edu>
2017-07-17 18:18:20 +0400

    Add files via upload

---
+++ Painting.java
@@ -0,0 +1,12 @@
+import tester.*;
+public class Painting{
+  String title;
+  String artist;
+  int year;
+  double price
+    public Painting(String title, String artist, int year, double price) {
+    this.title=title;
+    this.artist=artist;
+    this.year=year;
+    this.price=price;}}
+
